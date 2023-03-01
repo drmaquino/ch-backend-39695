62d8fae86959243bab4d09ea27c31c61a72a0c87
mariano aquino <mariano.aquino@gmail.com>
2023-03-01 12:07:49 -0300

    agrega material clase 12 mongodb

---
+++ clase-12-mongo-intro/ejemploBusquedaBinaria.js
@@ -0,0 +1,37 @@
+const personas = [
+    {
+        id: 1,
+        dni: 555,
+        nombre: 'bla'
+    },
+    {
+        id: 2,
+        dni: 222,
+        nombre: 'bla'
+    },
+    {
+        id: 3,
+        dni: 999,
+        nombre: 'bla'
+    },
+    {
+        id: 4,
+        dni: 333,
+        nombre: 'bla'
+    },
+    {
+        id: 5,
+        dni: 888,
+        nombre: 'bla'
+    },
+    {
+        id: 6,
+        dni: 111,
+        nombre: 'bla'
+    },
+    {
+        id: 7,
+        dni: 444,
+        nombre: 'bla'
+    },
+]
